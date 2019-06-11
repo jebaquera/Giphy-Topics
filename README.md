73d5d703c993d06b889f81bb0447aee034b2c46f
jebaquera <jebaquera@gmail.com>
2019-06-10 20:28:29 -0500

    updates to all files including README.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Giphy-Topics
-A site with topics and gifs pulled using the Giphy API.
+<bold>A site with topics and gifs pulled using the Giphy API.</bold>
+<br />
+The site contains button topics that have a Tropical theme to them. You can select from any of the pre-constructed search buttons to receive up to 10 animated GIFs for that particular topic. 
+<br />
+Clicking on one of the search items will animate the GIF. As well, you can hover over the GIF to animate it.
+<br />
+I have set the rating value to "g". With this rating you should expect returns rated with either "y" or "g".
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,65 @@
-var topics = ["palms", "toucans", "tropical", "macaws"]
+$( document ).ready(function() {
+    console.log("ready!");
+});
+
+// Inital array of tropical theme topics
+var tropicTitle = ["Palms", "Tropics", "Macaws", "Monkeys", "Amazonia", "Bananas", "Rainforest"];
+var currentGif; 
+var pausedGif; 
+var animatedGif; 
+var stillGif;
+
+
+// Creates theme or topic buttons from current array
+function createButtons(){
+	$('#tropicButtons').empty();
+	for(var i = 0; i < tropicTitle.length; i++){
+		var showBtn = $('<button>').text(tropicTitle[i]).addClass('showBtn').attr({'data-name': tropicTitle[i]});
+		$('#tropicButtons').append(showBtn);
+	}
+
+	// Displays gifs on tropicTitle button click event
+	$('.showBtn').on('click', function(){
+		$('.display').empty();
+
+        var thisTheme = $(this).data('name');
+        // api call limited to 10 returns and a rating of y
+		var giphyURL = "https://api.giphy.com/v1/gifs/search?&q=" + thisTheme + "&api_key=gEngaE1q51U3TQe1gnAPECsj3z8zLMqJ&limit=10&rating=g";
+		$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
+			currentGif = giphy.data;
+			$.each(currentGif, function(index,value){
+				animatedGif = value.images.original.url;
+				pausedGif = value.images.original_still.url;
+                var thisRating = value.rating;
+                
+				// gifs with blank ratings will get 'unrated' text
+				if(thisRating == ''){
+					thisRating = 'unrated';
+				}
+				var rating = $('<h5>').html('Rated: '+ thisRating).addClass('ratingStyle');
+				stillGif= $('<img>').attr('data-animated', animatedGif).attr('data-paused', pausedGif).attr('src', pausedGif).addClass('playOnHover');
+				var fullGifDisplay = $('<button>').append(rating, stillGif);
+				$('.display').append(fullGifDisplay);
+			});
+		});
+	});
+}
+
+
+// Animates and pauses gif on hover
+$(document).on('mouseover','.playOnHover', function(){
+    $(this).attr('src', $(this).data('animated'));
+    });
+    $(document).on('mouseleave','.playOnHover', function(){
+    $(this).attr('src', $(this).data('paused'));
+});
+
+// Creates a button from value that user inputs - will create a new theme button as well
+$('#addTheme').on('click', function(){
+    var newTheme = $('#newThemeInput').val().trim();
+    tropicTitle.push(newTheme);
+    createButtons();
+    return true;
+});
+
+createButtons();
